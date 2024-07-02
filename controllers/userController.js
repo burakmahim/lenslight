c71c796969afe348e3cea4be3cd026fc5bae7db8
burakmahim <burakmahim718@gmail.com>
2024-07-02 15:21:21 +0300

    Logout eklendi

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,10 +10,7 @@ const createUser = async (req, res) => {
 
     try {
         const user = await User.create(req.body)
-    res.status(201).json({
-        succeded: true,
-        user,
-    })
+    res.redirect("/login")
 
     } catch (error) {
         res.status(500).json({
